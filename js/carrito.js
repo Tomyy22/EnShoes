5a7378a1b864a5840accf19256e00dbdf9e5817a
TomyPlassy <plassytomas05@gmail.com>
2024-06-01 02:17:57 -0300

    fix link

--- js/carrito.js
+++ js/carrito.js
@@ -14,16 +14,18 @@ function generarLink() {
     if (productosEnCarrito && productosEnCarrito.length > 0) {
         const mensajeProductos = productosEnCarrito.map(producto => {
             const partesRutaImagen = producto.imagen.split('/');
-            const rutaCodificada = partesRutaImagen.map(parte => encodeURIComponent(parte)).join('/');
+            const rutaCodificada = partesRutaImagen.map(parte => encodeURIComponent(parte.replace(/ /g, '%20'))).join('/');
             
             return `Producto: ${producto.titulo} - Cantidad: ${producto.cantidad} - Talle: ${producto.talle} - Imagen: ${window.location.origin}/${rutaCodificada}`;
         }).join("%0A%0A"); // %0A%0A agrega dos saltos de línea entre los productos
         
-        const mensajeWhatsApp = `Quiero Comprar en En-Shoes 😎🔥👟%0A%0A${mensajeProductos}`;
-        const urlWhatsApp = `https://wa.me/543407441094?text=${mensajeWhatsApp}`;
+        const mensajeCodificado = encodeURIComponent(`Quiero Comprar en En-Shoes 😎🔥👟%0A%0A${mensajeProductos}`);
+        const urlWhatsApp = `https://wa.me/543407441094?text=${mensajeCodificado}`;
         document.querySelector(".link-comprar").setAttribute("href", urlWhatsApp);
     }
 }
+
+
 
 
 
